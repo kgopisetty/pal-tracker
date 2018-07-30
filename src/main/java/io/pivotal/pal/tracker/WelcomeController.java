a56c0d73654951ff9ebee233ccebc2a605deefac
pal_user <kirankumarg@hotmail.com>
2018-07-30 17:52:25 -0600

    second commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Component
 public class WelcomeController {
+
+
+    private String messageStr;
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return messageStr;
     }
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}")String str)
+    {
+        messageStr=str;
+    }
+
 }
--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -2,15 +2,18 @@ package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class WelcomeControllerTest {
 
     @Test
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
-
+        System.out.println("testttt:"+controller.sayHello());
         assertThat(controller.sayHello()).isEqualTo("A welcome message");
     }
 }
